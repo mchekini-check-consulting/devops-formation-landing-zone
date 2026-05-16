23959bd816a9d526f0a42e82078b3f90787c3394
juba-touam <jubatouam61@gmail.com>
2026-05-16 16:21:12 +0200

    add backend keycloak && policies for keycloak and rule outbund to communicate with keycloak vm

--- modules/hub/outputs.tf
+++ modules/hub/outputs.tf
@@ -29,7 +29,6 @@ output "key_vault_name" {
   value = azurerm_key_vault.main.name
 }
 
-<<<<<<< HEAD
 output "keycloak_private_ip" {
   description = "Private IP address of the Keycloak VM"
   value       = azurerm_network_interface.keycloak.ip_configuration[0].private_ip_address
@@ -49,10 +48,12 @@ output "keycloak_vm_name" {
   description = "Name of the Keycloak VM"
   value       = azurerm_linux_virtual_machine.keycloak.name
 }
+
 output "apim_public_ip" {
   description = "IP publique de l'APIM pour les tests depuis le navigateur"
   value       = azurerm_api_management.main.public_ip_addresses[0]
-=======
+}
+
 output "log_analytics_workspace_id" {
   description = "ID du Log Analytics Workspace centralisé pour tous les environnements"
   value       = azurerm_log_analytics_workspace.main.id
@@ -68,5 +69,4 @@ output "application_insights_connection_string" {
   description = "Connection string Application Insights (méthode recommandée pour les SDK récents)"
   value       = azurerm_application_insights.main.connection_string
   sensitive   = true
->>>>>>> 5e0a4db (feat: add Application Insights + Log Analytics Workspace in hub rg- monitoring)
 }
