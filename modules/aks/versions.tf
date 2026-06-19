510f64d914f87750350a4bed55abe453b8b892be
elGiordano <aguelid_aksel@gmail.com>
2026-06-19 02:54:00 +0200

    wip

--- modules/aks/versions.tf
+++ modules/aks/versions.tf
@@ -2,10 +2,6 @@ terraform {
   required_version = ">= 1.7"
 
   required_providers {
-    azuread = {
-      source  = "hashicorp/azuread"
-      version = "~> 3.0"
-    }
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 4.0"
