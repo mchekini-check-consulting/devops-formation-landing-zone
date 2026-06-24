5f4824b8688c4e35d752c26d6012ff3eed6b0808
elGiordano <aguelid_aksel@gmail.com>
2026-06-24 03:17:29 +0200

    fix: add required_version and provider constraints to all modules

--- modules/hub/providers.tf
+++ modules/hub/providers.tf
@@ -1,13 +1,13 @@
 terraform {
   required_version = ">= 1.5"
   required_providers {
-    azuread = {
-      source  = "hashicorp/azuread"
-      version = "~> 3.0"
-    }
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 4.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"
     }
   }
 }
