e621fad6d881cee878526493deb9e224d6be6e7c
Juba TOUAM <62524789+juba-touam@users.noreply.github.com>
2026-07-12 00:54:46 +0200

    refacto to exclure kubernetes ressources (#33)

--- modules/platform/outputs.tf
+++ modules/platform/outputs.tf
@@ -3,12 +3,3 @@ output "backup_identity_client_id" {
   value       = azurerm_user_assigned_identity.backup.client_id
 }
 
-output "sonarqube_namespace" {
-  description = "Namespace Kubernetes de SonarQube"
-  value       = helm_release.sonarqube.namespace
-}
-
-output "sonarqube_admin_secret_name" {
-  description = "Nom du secret Key Vault contenant le mot de passe admin SonarQube"
-  value       = azurerm_key_vault_secret.sonarqube_admin_password.name
-}
